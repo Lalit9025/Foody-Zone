3a913dadfac71eb7e259416919e2c3d3ed21093d
Lalit9025 <lalit3chouhan@gmail.com>
2024-01-20 16:37:44 +0530

    Initial Commit

---
+++ src/FoodData.js
@@ -0,0 +1,44 @@
+export const foodData = [
+    {
+        name: "Boilded Egg",
+        price: 10,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        image: "/Images/egg.png",
+        type: "breakfast",
+    },
+    {
+        name: "RAMEN",
+        price: 25,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        image: "/Images/ramen.png",
+        type: "lunch",
+    },
+    {
+        name: "GRILLED CHICKEN",
+        price: 45,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        image: "/Images/chicken.png",
+        type: "dinner",
+    },
+    {
+        name: "CAKE",
+        price: 18,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        image: "/Images/cake.png",
+        type: "breakfast",
+    },
+    {
+        name: "BURGER",
+        price: 23,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        image: "/Images/burger.png",
+        type: "lunch",
+    },
+    {
+        name: "PANCAKE",
+        price: 25,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+        image: "/Images/pancake.png",
+        type: "dinner",
+    },
+];
